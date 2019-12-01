5c2154f0f8c2f608380b66b2c036bafa61db6c43
Alex <TetrisIQ@web.de>
2019-12-01 16:44:21 +0100

    remove unused tests

--- src/test/kotlin/de/openmail/OpenmailApplicationTests.kt
+++ src/test/kotlin/de/openmail/OpenmailApplicationTests.kt
@@ -5,18 +5,10 @@ import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
+//@RunWith(SpringRunner::class)
+//@SpringBootTest
 class OpenmailApplicationTests {
 
-	@Test
-	fun contextLoads() {
-        print("Hallo Tests laufen!")
-	}
 
-    @Test
-    fun secoundTest() {
-        print("der 2te test läuft auch\n Yey" )
-    }
 
 }
